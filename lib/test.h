d9314679367ad49cf11ddbbc583b7a0ec53f7b73
TEC-Andres <a01287002@tec.mx>
2026-05-11 11:59:40 -0600

    Revert "initial commit"
    This reverts commit ab51a838eb7a1dbea56a5524344e55eadad18586.

--- lib/test.h
+++
@@ -1,9 +0,0 @@
-#pragma once 
-#include <iostream>
-
-class Test {
-public:
-    void runTests() {
-        std::cout << "Running tests..." << std::endl;
-    }
-};
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "test.h"
-
-int main() {
-    Test t;
-    t.runTests();
-    return 0;
-}
