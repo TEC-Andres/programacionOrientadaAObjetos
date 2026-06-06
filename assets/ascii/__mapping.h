8115a10e6682b56b6903e5b9d20bba8ad8669f0f
TEC-Andres <a01287002@tec.mx>
2026-06-06 17:28:14 -0600

    Added ascii concept reference

---
+++ assets/ascii/__mapping.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// -------------------------------------------------------
+// ASCII Art Registry
+// Include this file to access all registered ASCII art.
+// Each art asset is exposed as:
+//   - ui::ascii::<name>_raw[]    (string array of lines)
+//   - ui::ascii::<name>_lineCount (int)
+// -------------------------------------------------------
+
+#include "logo.h"
+
+// Future art assets will be added here:
+// #include "other_art.h"
